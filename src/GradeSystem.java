12f658b4f4a1af513d7b47cf0983ea2c71019287
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-23 20:47:25 +0200

    adding system essentials

--- src/GradeSystem.java
+++ src/GradeSystem.java
@@ -1,7 +1,21 @@
 import classes.Students;
+import java.util.*;
 
 public class GradeSystem{
     public static void main(String[] args){
+   Scanner scanner = new Scanner(System.in);
+   while(true) {
+       System.out.println("welcome to the student-MIS");
+       System.out.println("--------------------------------");
+       System.out.println("Enter the role ( teacher or Student ) :");
+       String role = scanner.nextLine();
+       if (role.equalsIgnoreCase("teacher")){
 
+       }else if(role.equalsIgnoreCase("student")){
+
+       }else{
+           System.out.println("Invalid role");
+       }
+   }
     }
 }
