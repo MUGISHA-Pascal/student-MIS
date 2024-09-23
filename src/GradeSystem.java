08ce94cde269174c267a43c96b6e60418200221a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-23 22:36:39 +0200

    upgrading the system

--- src/GradeSystem.java
+++ src/GradeSystem.java
@@ -16,24 +16,43 @@ public class GradeSystem{
            System.out.println("options available is :");
            System.out.println("1. add grade to a student");
            System.out.println("2. check the ranks of students");
+           System.out.println("3. add a new student");
            System.out.println("Enter the choice (number) :");
            int choice = scanner.nextLine();
            switch(choice) {
                case 1:
-
+              System.out.println("Enter the name of the student : ");
+              String name = scanner.nextLine();
+              for(Student student:students){
+              if(name.equalIgnoreCase(student.name)) {
+                  System.out.println("Enter the grade of the student : ");
+                  int grade = scanner.nextInt();
+                  student.grade=grade;
+              }
+              }
                    break;
-           }
-
-           Collections.sort(students,Comparator.comparingDouble(Student::getGrade).reversed());
-           for(Student student:students){
-               int i=0;
-           System.out.println(i++ + "-" + student.name);
+               case 2:
+                   Collections.sort(students,Comparator.comparingDouble(Student::getGrade).reversed());
+                   for(Student student:students){
+                       int i=0;
+                       System.out.println(i++ + "-" + student.name);
+                   }
+                   break;
+               case 3:
+                   System.out.println("Enter the name of the new student : ");
+                   String name = scanner.nextLine();
+                   System.out.println("Enter the class name of the new student : ");
+                   String className = scanner.nextLine();
+                   break;
+                   default:
+                       System.out.println("Invalid choice");
+                       break;
            }
        }else if(role.equalsIgnoreCase("student")){
            System.out.println("welcome to student management");
            System.out.println("----------------------------------");
            System.out.println("The options available :");
-           System.out.println("1. ");
+           System.out.println("1. check rank");
        }else{
            System.out.println("Invalid role");
        }
--- src/classes/Student.java
+++ src/classes/Student.java
@@ -1,6 +1,7 @@
 package classes;
 public class Student{
    public String name;
+   public String className;
    public int rank;
    public Double Grade;
 
