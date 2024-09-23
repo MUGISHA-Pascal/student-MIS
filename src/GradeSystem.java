2a96c565f17ff2b92b7337cf586866fad4dd13db
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-23 22:20:39 +0200

    upgrading the system

--- src/GradeSystem.java
+++ src/GradeSystem.java
@@ -1,21 +1,41 @@
-import classes.Students;
+import classes.Student;
 import java.util.*;
 
 public class GradeSystem{
     public static void main(String[] args){
    Scanner scanner = new Scanner(System.in);
+   ArrayList<Student> students = new ArrayList<>();
    while(true) {
        System.out.println("welcome to the student-MIS");
        System.out.println("--------------------------------");
        System.out.println("Enter the role ( teacher or Student ) :");
        String role = scanner.nextLine();
        if (role.equalsIgnoreCase("teacher")){
+           System.out.println("welcome to teacher management");
+           System.out.println("---------------------------------");
+           System.out.println("options available is :");
+           System.out.println("1. add grade to a student");
+           System.out.println("2. check the ranks of students");
+           System.out.println("Enter the choice (number) :");
+           int choice = scanner.nextLine();
+           switch(choice) {
+               case 1:
 
+                   break;
+           }
+
+           Collections.sort(students,Comparator.comparingDouble(Student::getGrade).reversed());
+           for(Student student:students){
+               int i=0;
+           System.out.println(i++ + "-" + student.name);
+           }
        }else if(role.equalsIgnoreCase("student")){
-
+           System.out.println("welcome to student management");
+           System.out.println("----------------------------------");
+           System.out.println("The options available :");
+           System.out.println("1. ");
        }else{
            System.out.println("Invalid role");
        }
    }
     }
-}
--- src/classes/Student.java
+++ src/classes/Student.java
@@ -1,8 +1,10 @@
 package classes;
 public class Student{
    public String name;
-   public String className;
    public int rank;
-   public int averageGrades;
+   public Double Grade;
 
+   public Double getGrade() {
+      return Grade;
+   }
 }
